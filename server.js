0d260db37d88a8b774f85c0a81fd064203ab66b1
Eric Fetiveau <eric.fetiveau@gmail.com>
2012-04-19 23:45:47 +0200

    send 2 can message

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var receivedqueue = [];
 new Board({
     debug: true,
     portUSB: "/dev/ttyUSB0",
-    baudrate: 57600,
+    baudrate: 115200,
     sentqueue: sentqueue,
     receivedqueue: receivedqueue
 });
@@ -19,7 +19,8 @@ var f1 = function() {
         var test1 = new Ahapacket();
 
         test1.createCanMessage(1, 0, 2, 15, 0, 12, 0);
-        test1.toString();
+        logule.debug(test1.toString());
+        sentqueue.push(test1);
         sentqueue.push(test1);
     };
 
